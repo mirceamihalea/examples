91425df18c37205ba3d247e19eb5818cc5df614d
Mircea Mihalea <fortech.mihalea@extaccount.com>
2024-11-13 09:10:15 +0200

    remove unnecessary code

--- testing-unit-py/__main__.py
+++ testing-unit-py/__main__.py
@@ -2,8 +2,4 @@ import pulumi
 
 import infra
 
-pulumi.export("group", infra.group)
-pulumi.export("server", infra.server)
-pulumi.export("publicIp", infra.server.public_ip)
-pulumi.export("publicHostName", infra.server.public_dns)
 pulumi.export("account", infra.account)
